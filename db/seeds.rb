7cae2316918d1240e14204d57fea87b1717cb9b0
Daniela Santana <daanims@gmail.com>
2020-05-13 13:49:03 -0400

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def base_restaurant 
+  {
+    address: "montreal",
+    phone_number: "514 111 1111"
+  }  
+end
+
+Restaurant.create(base_restaurant.merge({name: "italiano", category: "italian"}))
+Restaurant.create(base_restaurant.merge({name: "chino", category: "chinese"}))
+Restaurant.create(base_restaurant.merge({name: "japan", category: "japanese"}))
+Restaurant.create(base_restaurant.merge({name: "france", category: "french"}))
+Restaurant.create(base_restaurant.merge({name: "belgo", category: "belgian"}))
+
+##5.times do |i|
+#  Restaurant.create(name: 'italianissimo', address: 'montreal' , { phone_number: '514 555 2525' } { category: 'italian' }])
+#end
