66c3adaf1de38e42af866d7d8148a6f65fa7cf3b
Daniela Santana <daanims@gmail.com>
2020-05-13 14:58:39 -0400

    fixing some of the frontend issues

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.after_save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+  
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   # view all restaurants
-  get "/restaurants", to "restaurants#index"
+  get "/restaurants", to: "restaurants#index"
 
   # add a new restaurant, and be redirected to the show view of that new restaurant.
-  get "/restaurants/new", to "restaurants#new", as: :new_restaurant
-  post "/restaurants", to "restaurants#create"
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "/restaurants", to: "restaurants#create"
   
   # see the details of a restaurant, with all the reviews related to the restaurant.
-  get "/restaurants/:id", to "restaurants#show", as: :restaurant
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
 
   # add a new review to a restaurant
-  get "/restaurants/:id/reviews/new", to ""
-  post "/restaurants/:id/reviews", to ""
+  get "/restaurants/:id/reviews/new", to: "reviews#new"
+  post "/restaurants/:id/reviews", to: "reviews#create"
+
 end
